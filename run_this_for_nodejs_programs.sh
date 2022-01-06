9fad1309af797cb943d5c25a8617ab454b081f66
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-06 23:43:20 +0200

    Create run_this_for_nodejs_programs.sh

---
+++ run_this_for_nodejs_programs.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+## PROGRAM FOR CREATING NODE JS APPLICATIONS
+
+WORKDIR="${REPO_NODEJS}" ; 
+echo ">> Current PWD => $WORKDIR" ; 
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function FUNC_CREATE_NODEJS_PROJECT () {
+    echo ">> ENTER Name of the NODE JS APP project folder to create : " ; 
+    read node_app_name ;
+    APPDIR="$WORKDIR/$node_app_name" ; 
+    mkdir -p "$APPDIR" ;
+    echo ">> CD'ing to the app folder ..." ; 
+    cd $APPDIR ; 
+    echo "** Current PWD = $(pwd) **" ; 
+    echo ">> CREATING NODE APP USING => npm init -y" ;
+    npm init -y ; ## chooses all default options 
+    echo ">> NODE-JS app created. Contents of app folder listed below ..." ; 
+    ls -al "$APPDIR" ; 
+}
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+echo ">> SELECT THE OPTION NUMBER FROM BELOW: " ; 
+
+select myOptn in "Create the NodeJS app" "Run the NodeJS app locally" ;  do
+    echo "You have chosen => $myOptn" ;
+    ##++++++++++++
+    case $myOptn in
+    "Create the NodeJS app")
+    FUNC_CREATE_NODEJS_PROJECT ; 
+    ;;
+    "Run the NodeJS app locally")
+    ## RUNNING THE APP LOCALLY
+    echo ">> RUNNING THE APP LOCALLY ..."  ; 
+    npm index.js ; 
+    ;;
+    *)
+    echo "*** Invalid entry. Breaking out. ***"
+    break ;
+    ;;
+    esac
+    ##++++++++++++
+done
