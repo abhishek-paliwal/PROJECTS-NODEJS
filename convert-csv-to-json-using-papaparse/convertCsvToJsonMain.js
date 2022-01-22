f06cf9df9105bd2c8383bf811f8b4075a157cd4b
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-22 16:59:48 +0200

    project initialized => convert csv to json using papapaperse

---
+++ convert-csv-to-json-using-papaparse/convertCsvToJsonMain.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const Papa = require('papaparse');
+
+/* =========================================================== */
+// CONCATENATING ALL CSV FILES INTO ONE CSV FILE
+baseDir = process.env.REPO_NODEJS + 'convert-csv-to-json-using-papaparse/' ; 
+inDir = baseDir + 'data_input/' ; 
+outDir = baseDir + 'data_output/' ; 
+outFileFinal = outDir + 'combinedFinalFile.csv' ; 
+outFileFinalJson = outDir + 'combinedFinalFile.json' ; 
+const combinedFileData = require('child_process').execSync('cat ' + inDir + '*.csv').toString('UTF-8') ; 
+fs.writeFileSync(outFileFinal, combinedFileData) ; 
+/* =========================================================== */
+
+/* =========================================================== */
+// FUNCTION TO WRITE CSV DATA TO A JSON FILE
+function writeToJsonFile(myDataObject) {
+    try {
+    fs.writeFileSync(outFileFinalJson, JSON.stringify(myDataObject)) ; 
+    console.log('file written successfully') ; 
+    } catch (err) {
+    console.error(err) ; 
+    }
+}
+/* =========================================================== */
+
+/* =========================================================== */
+// USE PAPA PARSE TO READ CSV FILE DATA
+const csvFilePath = outFileFinal ; 
+const file = fs.createReadStream(csvFilePath) ;
+/* ====== */
+Papa.parse(file, {
+  header: true,
+  skipEmptyLines: false,
+  complete: function(results) {
+    console.log('=========================================='); 
+    console.log('Complete', results.data.length, 'records.'); 
+    console.log('=========================================='); 
+    console.log(results.data); 
+    console.log('=========================================='); 
+    writeToJsonFile(results.data) ;
+    console.log('=========================================='); 
+    for (let singleRow of results.data.entries()) {
+      try {
+      console.log('<a href="' + singleRow[1]['CONTENT TYPE'] + '">' + singleRow[1].ABBR + '</a>');
+      }
+      catch(error) {
+          console.error(error) ;
+      }
+    }
+  } 
+});
+/* =========================================================== */
